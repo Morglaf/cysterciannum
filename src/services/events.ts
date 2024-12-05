a16d2299afe5777fa2390fb8e5f8c8d22ca2b63e
Robin <robin@codecolliders.com>
2024-12-05 13:41:16 +0100

    Fix deployment configuration 32

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,4 +1,25 @@
-import EventEmitter from 'events';
+type EventCallback = (data: any) => void;
+
+class EventEmitter {
+  private listeners: { [key: string]: EventCallback[] } = {};
+
+  on(event: string, callback: EventCallback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: EventCallback) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+  }
+
+  emit(event: string, data: any) {
+    if (!this.listeners[event]) return;
+    this.listeners[event].forEach(callback => callback(data));
+  }
+}
 
 export const EVENTS = {
   XP_UPDATED: 'xp_updated'
